430527ea360fde90259e8b0cd36c125375326346
Phil Barow <pbarow@gmail.com>
2017-02-21 19:00:41 -0800

    saving for teaching Terrance

--- hellofile.js
+++ hellofile.js
@@ -19,19 +19,92 @@ var player1 = '';
 var player2 = '';
 
 
-console.log('What is the name of Player 1: ');
 rl.prompt();
+console.log('What is the name of Player 1: ');
+
 rl.on('line', (input) => {
   console.log(`Received: ${input}`);
   player1 = input;
-});
+    rl.prompt();
+    console.log('What is the name of Player 2: ');
+    rl.on('line', (input) => {
+    
+
+    console.log(`Received: ${input}`);
+     player2 = input;
+  
+var board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+
+  printBoard(board);
 
 
-console.log('What is the name of Player 2: ');
-rl.on('line', (input) => {
-  console.log(`Received: ${input}`);
-  player2 = input;
-});
+var playing = true;
+var playerAPlaying = true;
+var playerBPlaying = true;
+
+
+
+// while (playing) {
+
+
+//   while (playerAPlaying) {
+//     console.log(player1 + ' please enter a number to place you X');
+    
+//     rl.on('line', (input) => {
+
+//       row = Math.floor((input - 1 ) / 3);
+//       column = (input - 1 ) % 3 ;
+
+
+//       if (typeof board[row][column] === 'number') {
+//         board[row][column] = 'X';
+//         // check for tic tac tow
+//         if (checkForTicTacToe('X')){
+//           console.log('Congrats ' + player1 + ',  you won!');
+//           rl.close();
+//           playing = false;
+//         }
+//         playerAPlaying = false;
+//       } else {
+//         console.log('That is not a valid move, try again');
+//       }
+
+//     });
+//   }
+
+//   while (playerBPlaying) {
+//     console.log(player2 + ' please enter a number to place you X');
+    
+//     rl.on('line', (input) => {
+
+//       row = Math.floor((input - 1 ) / 3);
+//       column = (input - 1 ) % 3 ;
+
+
+//       if (typeof board[row][column] === 'number') {
+//         board[row][column] = 'O';
+//         // check for tick tac toe
+//         if ( checkForTicTacToe('O')) {
+//           console.log('Congrats ' + player2 + ',  you won!');
+//           rl.close();
+//           playing = false;
+//         }
+
+//         playerBPlaying = false;
+//       } else {
+//         console.log('That is not a valid move, try again');
+//       }
+
+//     });
+//   }
+// }
+
+
+  });
 
 function printBoard(board) {
 
@@ -43,75 +116,11 @@ function printBoard(board) {
 }
 
 
-  var board = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-  ];
+  
 
-  printBoard(board);
+});
 
 
-var playing = false;
-var playerAPlaying = false;
-var playerBPlaying = false;
-
-
-if ()
-while (playing) {
-
-
-  while (playerAPlaying) {
-    console.log(player1 + ' please enter a number to place you X');
-    
-    rl.on('line', (input) => {
-
-      row = Math.floor((input - 1 ) / 3);
-      column = (input - 1 ) % 3 ;
-
-
-      if (typeof board[row][column] === 'number') {
-        board[row][column] = 'X';
-        // check for tic tac tow
-        if (checkForTicTacToe('X')){
-          console.log('Congrats ' + player1 + ',  you won!');
-          rl.close();
-          playing = false;
-        }
-        playerAPlaying = false;
-      } else {
-        console.log('That is not a valid move, try again');
-      }
-
-    });
-  }
-
-  while (playerBPlaying) {
-    console.log(player2 + ' please enter a number to place you X');
-    
-    rl.on('line', (input) => {
-
-      row = Math.floor((input - 1 ) / 3);
-      column = (input - 1 ) % 3 ;
-
-
-      if (typeof board[row][column] === 'number') {
-        board[row][column] = 'O';
-        // check for tick tac toe
-        if ( checkForTicTacToe('O')) {
-          console.log('Congrats ' + player2 + ',  you won!');
-          rl.close();
-          playing = false;
-        }
-
-        playerBPlaying = false;
-      } else {
-        console.log('That is not a valid move, try again');
-      }
-
-    });
-  }
-}
 
 
 function checkForTicTacToe (player) {
---
+++ ttt.js
@@ -0,0 +1,16 @@
+//ttt.js
+//document.getElementsByClassName(names)
+
+// var box = document.getElementsByClassName('box');
+// console.log(box);
+
+// box[0].onclick = function (event) {
+//   alert('Eye of the tiger!');
+// };
+
+var box = document.getElementsByClassName('box');
+console.log(box);
+
+box[0].onclick = function (event) {
+  alert('Eye of the tiger!');
+};
